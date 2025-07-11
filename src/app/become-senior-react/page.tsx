13c8a9f1633613347cda5eb7a4f5aebea2d3bd96
Konan <vudinhdang2004tb@gmail.com>
2025-07-11 17:01:08 +0700

    fail

--- src/app/become-senior-react/page.tsx
+++ src/app/become-senior-react/page.tsx
@@ -13,9 +13,9 @@ import NhungGiBanSeHocDuoc from "./components/NhungGiBanSeHocDuoc";
 import EndScrollingLightSpots from "./components/EndScrollingLightSpots";
 import AnimatedPathSvg from "./components/AnimatedPathSvg";
 import ImageOptimized from "@/app/components/ImageOptimized";
-//import IframeModelViewer from "./components/IframeModelViewer";
-import ThreeModelViewer from "./components/ThreeModelViewer";
-// Không cần import isMobile cho layout nữa
+import IframeModelViewer from "./components/IframeModelViewer";
+//import ThreeModelViewer from "./components/ThreeModelViewer";
+
 
 const BecomeSeniorReactPage = () => {
   return (
@@ -52,8 +52,8 @@ const BecomeSeniorReactPage = () => {
           */}
           <div className="hidden lg:relative lg:block h-full">
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              {/* <ThreeModelViewer className="w-[1500px] h-[1500px] max-w-none max-h-none" /> */}
-              <ThreeModelViewer className="w-[1500px] h-[1500px] max-w-none max-h-none" />
+              <IframeModelViewer className="w-[1500px] h-[1500px] max-w-none max-h-none" />
+              {/* <ThreeWorker modelUrl="/images/model27.glb" className="w-[1500px] h-[1500px] max-w-none max-h-none" /> */}
             </div>
           </div>
         </main>
